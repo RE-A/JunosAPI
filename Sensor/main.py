5ed7242c7d5bf8d38cf381806b9398dc556e0f59
RE-A <skyseven73@gmail.com>
2021-01-29 01:27:41 +0900

    #3 fix: updatedAt 값 추가

--- Sensor/main.py
+++ Sensor/main.py
@@ -12,9 +12,10 @@ measure_data = sensor_new.measure()
 
 temperature_data = f'\'{measure_data["temperature"]}\'' or 'NULL'
 humidity_data = f'\'{measure_data["humidity"]}\'' or 'NULL'
+created_date = {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}
 
-sql = f"INSERT INTO TempAndHum  (temperature, humidity, createdAt) " \
-      f"VALUES ({temperature_data}, {humidity_data}, '{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}')"
+sql = f"INSERT INTO TempAndHum  (temperature, humidity, createdAt, updatedAt) " \
+      f"VALUES ({temperature_data}, {humidity_data}, '{created_date}', '{created_date}')"
 
 cur.execute(sql)
 conn.commit()
