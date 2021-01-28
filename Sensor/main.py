6627046f8454989503a7d88e5a2e2d137b72ec98
RE-A <skyseven73@gmail.com>
2021-01-29 01:24:35 +0900

    #3 fix: Sensor에서 DB에 데이터 입력시 DB 이름 오타 수정

--- Sensor/main.py
+++ Sensor/main.py
@@ -13,7 +13,7 @@ measure_data = sensor_new.measure()
 temperature_data = f'\'{measure_data["temperature"]}\'' or 'NULL'
 humidity_data = f'\'{measure_data["humidity"]}\'' or 'NULL'
 
-sql = f"INSERT INTO tempandhum  (temperature, humidity, createdAt) " \
+sql = f"INSERT INTO TempAndHum  (temperature, humidity, createdAt) " \
       f"VALUES ({temperature_data}, {humidity_data}, '{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}')"
 
 cur.execute(sql)
